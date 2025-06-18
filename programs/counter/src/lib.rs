c4b2add0ebd449cf1931834abed619eb7827e020
thrishank <thrishankkalluru@gmail.com>
2025-06-18 11:19:59 +0530

    compileing but signer error

--- programs/counter/src/lib.rs
+++ programs/counter/src/lib.rs
@@ -1,16 +1,20 @@
 #![allow(unexpected_cfgs)]
 use anchor_lang::prelude::*;
 
+use light_sdk::instruction::PackedAddressTreeInfo;
 use light_sdk::{
     account::LightAccount,
     address::v1::derive_address,
-    cpi::{CpiAccounts, CpiInputs},
-    instruction::{
-        account_meta::CompressedAccountMeta, merkle_context::PackedAddressMerkleContext,
-    },
-    LightDiscriminator, LightHasher, NewAddressParamsPacked, ValidityProof,
+    cpi::{CpiAccounts, CpiInputs, CpiSigner},
+    derive_light_cpi_signer,
+    instruction::{account_meta::CompressedAccountMeta, ValidityProof},
+    LightDiscriminator, LightHasher,
 };
-declare_id!("6d5t24rpiABQaFbVxa6iyBj2fc38y7wFVUrKYYBX2nWi");
+
+declare_id!("3Vzvt2ZHPUhaK6iDuvd1zC8oKBQwmazJ713aJGmiUiXs");
+
+pub const LIGHT_CPI_SIGNER: CpiSigner =
+    derive_light_cpi_signer!("3Vzvt2ZHPUhaK6iDuvd1zC8oKBQwmazJ713aJGmiUiXs");
 
 #[program]
 pub mod counter {
@@ -20,32 +24,33 @@ pub mod counter {
     pub fn create_counter<'info>(
         ctx: Context<'_, '_, '_, 'info, GenericAnchorAccounts<'info>>,
         proof: ValidityProof,
-        address_merkle_context: PackedAddressMerkleContext,
+        address_tree_info: PackedAddressTreeInfo,
         output_merkle_tree_index: u8,
     ) -> Result<()> {
-        let program_id = crate::ID.into();
+        let program_id = crate::ID;
         let light_cpi_accounts = CpiAccounts::new(
             ctx.accounts.signer.as_ref(),
             ctx.remaining_accounts,
-            crate::ID,
-        )
-        .map_err(ProgramError::from)?;
+            crate::LIGHT_CPI_SIGNER,
+        );
 
         let (address, address_seed) = derive_address(
-            &[b"counter", ctx.accounts.signer.key().as_ref()],
-            &light_cpi_accounts.tree_accounts()
-                [address_merkle_context.address_merkle_tree_pubkey_index as usize]
-                .key(),
+            &[b"counter123sf", ctx.accounts.signer.key().as_ref()],
+            &address_tree_info
+                .get_tree_pubkey(&light_cpi_accounts)
+                .map_err(|_| ErrorCode::AccountNotEnoughKeys)?,
             &crate::ID,
         );
 
-        let new_address_params = NewAddressParamsPacked {
-            seed: address_seed,
-            address_queue_account_index: address_merkle_context.address_queue_pubkey_index,
-            address_merkle_tree_root_index: address_merkle_context.root_index,
-            address_merkle_tree_account_index: address_merkle_context
-                .address_merkle_tree_pubkey_index,
-        };
+        let new_address_params = address_tree_info.into_new_address_params_packed(address_seed);
+
+        // let new_address_params = NewAddressParamsPacked {
+        //     seed: address_seed,
+        //     address_queue_account_index: address_merkle_context.address_queue_pubkey_index,
+        //     address_merkle_tree_root_index: address_merkle_context.root_index,
+        //     address_merkle_tree_account_index: address_merkle_context
+        //         .address_merkle_tree_pubkey_index,
+        // };
 
         let mut counter = LightAccount::<'_, CounterAccount>::new_init(
             &program_id,
--- tests/counter.ts
+++ tests/counter.ts
@@ -4,15 +4,12 @@ import { Counter } from "../target/types/counter";
 import {
   bn,
   createRpc,
-  defaultTestStateTreeAccounts2,
   deriveAddress,
   deriveAddressSeed,
-  hashToBn254FieldSizeBe,
-  NewAddressParams,
 } from "@lightprotocol/stateless.js";
+import { getKeypairFromFile } from "@solana-developers/helpers";
 
 describe("counter", () => {
-  // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.counter as Program<Counter>;
@@ -21,19 +18,27 @@ describe("counter", () => {
     "https://devnet.helius-rpc.com/?api-key=c991f045-ba1f-4d71-b872-0ef87e7f039d";
   const connection = createRpc(url, undefined, undefined);
 
-  const addressTree = defaultTestStateTreeAccounts2().merkleTree2;
-  const addressQueue = defaultTestStateTreeAccounts2().nullifierQueue2;
+  const addressTree = new anchor.web3.PublicKey(
+    "smt1NamzXdq4AMqS2fS2F1i5KTYPZRhoHgWx38d8WsT"
+  );
+  const addressQueue = new anchor.web3.PublicKey(
+    "nfq1NvQDJ2GEgnS8zt9prAe8rjjpAW1zFkrvZoBR148"
+  );
 
-  const randomBytes = anchor.web3.Keypair.generate().publicKey.toBytes();
   it("create", async () => {
+    const keypair: anchor.web3.Signer = await getKeypairFromFile();
+
     const seeds: Uint8Array[] = [
-      Buffer.from("counter"),
-      new anchor.web3.PublicKey(
-        "thrbabBvANwvKdV34GdrFUDXB6YMsksdfmiKj2ZUV3a"
-      ).toBuffer(),
+      Buffer.from("counter123sf"),
+      keypair.publicKey.toBuffer(),
     ];
-    const assetSeed = deriveAddressSeed(seeds, new anchor.web3.PublicKey(""));
+    const assetSeed = deriveAddressSeed(
+      seeds,
+      new anchor.web3.PublicKey("3Vzvt2ZHPUhaK6iDuvd1zC8oKBQwmazJ713aJGmiUiXs")
+    );
     const assetAddress = deriveAddress(assetSeed, addressTree);
+
+    // const at = connection.getAddressTree;
     const proof = await connection.getValidityProofV0(undefined, [
       {
         address: bn(assetAddress.toBytes()),
@@ -43,5 +48,98 @@ describe("counter", () => {
     ]);
 
     const validityProof = proof.compressedProof;
+
+    /// "smtAvYA5UbTRyKAkAj5kHs1CmrA42t6WkVLi4c6mA1f's signer privilege escalated",
+    const remainingAccounts = [
+      {
+        pubkey: new anchor.web3.PublicKey(
+          "SySTEM1eSU2p4BGQfQpimFEWWSC1XDFeun3Nqzz3rT7"
+        ), // Light System Program
+        isSigner: false,
+        isWritable: false,
+      },
+      {
+        pubkey: new anchor.web3.PublicKey(
+          "smt1NamzXdq4AMqS2fS2F1i5KTYPZRhoHgWx38d8WsT"
+        ), // Address Tree
+        isSigner: false,
+        isWritable: true,
+      },
+      {
+        pubkey: new anchor.web3.PublicKey(
+          "amt1Ayt45jfbdw5YSo7iz6WZxUmnZsQTYXy82hVwyC2"
+        ),
+        isSigner: false,
+        isWritable: true,
+      },
+      {
+        pubkey: new anchor.web3.PublicKey(
+          "aq1S9z4reTSQAdgWHGD2zDaS39sjGrAxbR31vxJ2F4F"
+        ), // Address Queue
+        isSigner: false,
+        isWritable: true,
+      },
+      {
+        pubkey: new anchor.web3.PublicKey(
+          "ComputeBudget111111111111111111111111111111"
+        ), // Compute Budget
+        isSigner: false,
+        isWritable: false,
+      },
+      {
+        pubkey: new anchor.web3.PublicKey(
+          "35hkDgaAKwMCaxRz2ocSZ6NaUrtKkyNqU6c4RV3tYJRh"
+        ), // Registered Program PDA
+        isSigner: false,
+        isWritable: false,
+      },
+      {
+        pubkey: new anchor.web3.PublicKey(
+          "noopb9bkMVfRPU8AsbpTUg8AQkHtKwMYZiFUjNRtMmV"
+        ), // Noop Program
+        isSigner: false,
+        isWritable: false,
+      },
+      {
+        pubkey: new anchor.web3.PublicKey(
+          "HwXnGK3tPkkVY6P439H2p68AxpeuWXd5PcrAxFpbmfbA"
+        ), // Merkle Tree (writable)
+        isSigner: false,
+        isWritable: true,
+      },
+      {
+        pubkey: new anchor.web3.PublicKey(
+          "compr6CUsB5m2jS4Y3831ztGSTnDpnKJTKS95d64XVq"
+        ), // Compression Program
+        isSigner: false,
+        isWritable: false,
+      },
+      {
+        pubkey: new anchor.web3.PublicKey("11111111111111111111111111111111"), // System Program
+        isSigner: false,
+        isWritable: false,
+      },
+    ];
+
+    await program.methods
+      .createCounter(
+        {
+          0: {
+            a: validityProof.a,
+            b: validityProof.b,
+            c: validityProof.c,
+          },
+        },
+        {
+          addressMerkleTreePubkeyIndex: 1,
+          addressQueuePubkeyIndex: 2,
+          rootIndex: proof.rootIndices[0],
+        },
+        1
+      )
+      .accounts({ signer: keypair.publicKey })
+      .remainingAccounts(remainingAccounts)
+      .signers([keypair])
+      .rpc();
   });
 });
